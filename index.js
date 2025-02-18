c96e322aaccccdbce335f52fd4c05203d5d1ada4
FTWinston <ftwinston@gmail.com>
2025-02-18 21:19:07 +0000

    Adapt a minimal page appearance

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ let audio = null;
 let revAudio = null;
 let recorder = null;
 
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioContext = new AudioContext();
+let audioContext = null;
 
 function startRecording() {
+  if (audioContext === null) {
+    audioContext = new AudioContext();
+  }
+
   recorder = new Promise(resolve => {
-    const startButton = document.getElementById("start");
-    const stopButton = document.getElementById("stop");
-    startButton.disabled = true;
-    stopButton.disabled = false;
     let chunks = [];
   
     navigator.mediaDevices.getUserMedia({ audio: true }).then(
@@ -34,47 +33,72 @@ let samples = [];
 let sr = 44100;
 
 function stopRecording() {
-  const startButton = document.getElementById("start");
-  const stopButton = document.getElementById("stop");
-  startButton.disabled = false;
-  stopButton.disabled = true;
-
   mediaRecorder.stop();
   recorder.then(chunks => {
     audioBlob = new Blob(chunks, {type:'audio/mp3'});
     const audioUrl = URL.createObjectURL(audioBlob);
     audio = new Audio(audioUrl);
-    // Now plot and make reversed audio
+
     audioBlob.arrayBuffer().then(
       buffer => {
         audioContext.decodeAudioData(buffer, function(buff) {
           sr = buff.sampleRate;
           samples= buff.getChannelData(0);
-          let xs = [];
-          let ys = [];
-          for (let i = 0; i < samples.length; i++) {
-            xs.push(i/sr);
-            ys.push(samples[i]);
-          }
-          let plot = {x:xs, y:ys}
-          let layout = {title:'Audio samples',
-                        autosize: false,
-                        width: 800,
-                        height: 400};
-          Plotly.newPlot("audioPlot", [plot], layout);
         });
-        
       }
     );
   });
-
 }
 
-function playAudio() {
+let recording = false;
+
+function startStopRecording() {
+  recording = !recording;
+
+  document.getElementById("startStop").classList.toggle('button--active', recording);
+  document.getElementById("play").disabled = recording;
+  document.getElementById("playRev").disabled = recording;
+
+  if (recording) {
+    startRecording();
+  }
+  else {
+    stopRecording();
+  }
+}
+
+let playing = false;
+
+function playForwards() {
+  if (playing) {
+    return;
+  }
+
+  playing = true;
+  document.getElementById("play").classList.add('button--active');
+  document.getElementById("startStop").disabled = true;
+  document.getElementById("playRev").disabled = true;
+
   audio.play();
+  
+  audio.onended = () => {
+    playing = false;
+    document.getElementById("play").classList.remove('button--active');
+    document.getElementById("startStop").disabled = false;
+    document.getElementById("playRev").disabled = false;
+  };
 }
 
-function playReversed() {
+function playBackwards() {
+  if (playing) {
+    return;
+  }
+
+  playing = true;
+  document.getElementById("playRev").classList.add('button--active');
+  document.getElementById("startStop").disabled = true;
+  document.getElementById("play").disabled = true;
+
   const N = samples.length;
   let myArrayBuffer = audioContext.createBuffer(1, N, sr);
   let audio = myArrayBuffer.getChannelData(0);
@@ -82,48 +106,15 @@ function playReversed() {
     audio[i] = samples[N-i-1];
   }
   let source = audioContext.createBufferSource();
-  // set the buffer in the AudioBufferSourceNode
   source.buffer = myArrayBuffer;
-  // connect the AudioBufferSourceNode to the
-  // destination so we can hear the sound
   source.connect(audioContext.destination);
-  // start the source playing
+
+  source.onended = () => {
+    playing = false;
+    document.getElementById("playRev").classList.remove('button--active');
+    document.getElementById("startStop").disabled = false;
+    document.getElementById("play").disabled = false;
+  };
+
   source.start();
-}
-
-function download() {
-  const url = window.URL.createObjectURL(audioBlob);
-  const a = document.createElement('a');
-  a.style.display = 'none';
-  a.href = url;
-  a.download = 'audio.mp3';
-  document.body.appendChild(a);
-  a.click();
-}
-
-
-
-function downloadReversed() {
-  let N = samples.length;
-  // interleaved
-  const audio = new Float32Array(N);
-  for (let i = 0; i < N; i++) {
-    audio[i] = samples[N-i-1];
-  }
-  // get WAV file bytes and audio params of your audio source
-  const wavBytes = getWavBytes(audio.buffer, {
-    isFloat: true,       // floating point or 16-bit integer
-    numChannels: 1,
-    sampleRate: sr,
-  })
-  const wav = new Blob([wavBytes], { type: 'audio/wav' });
-  
-  // create download link and append to Dom
-  const a = document.createElement('a');
-  a.href = window.URL.createObjectURL(wav);
-  a.style.display = 'none';
-  a.download = 'audioRev.wav';
-  document.body.appendChild(a);
-  a.click();
-
 }
